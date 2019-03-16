2c1b6bdbc115ba2425c1ea003cfe59faabee7e11
irunga1 <jirungaray@redpronet.com>
2019-03-15 20:32:38 -0600

    cambio formulario y controlador

--- src/controllers/OperacionController.js
+++ src/controllers/OperacionController.js
@@ -1,6 +1,7 @@
 'use strict'
 //var  gcds = require('@google-cloud/datastore');
 
+
 const {Storage} = require('@google-cloud/storage');
 
 // Instantiates a client. If you don't specify credentials when constructing
@@ -11,13 +12,16 @@ const {Storage} = require('@google-cloud/storage');
 
 function operate(req, res){
     var params = req.body;
-    //console.log("entro a post");
+    
     console.log(params);
-    if(params.Nombre !==""){
+    
+    
+    if((params.Nombre !=="") && (params.Apellido !=="") && (params.Telefono !=="")&& (params.Edad !=="")){
         res.status(200).send({message:"entro a post"});
-       
+        var btoa = require('btoa');
+        //console.log(params);
         const {Datastore} = require('@google-cloud/datastore');
-        async function quickStart() {
+        async function quickStart(data1) {
             // Your Google Cloud Platform project ID
             const projectId = 'cloudit4data';
             // Creates a client
@@ -27,27 +31,31 @@ function operate(req, res){
         // The kind for the new entity
         const kind = 'Task2';
         // The name/ID for the new entity
-        const name = 'sampletask1';
+        var a = new Date();
+        var b = btoa(a);
+        const name = b;
         // The Cloud Datastore key for the new entity
         const taskKey = datastore.key([kind, name]);
         // Prepares the new entity
+        // const task = {
+        //     key: taskKey,
+        //     data: {
+        //         Apellido: 'Irungaray2',
+        //         Edad:36,
+        //         Nombre: 'Juanito',
+        //         Telefono:'42142727'
+        //     },
+        // };
         const task = {
             key: taskKey,
-            data: {
-                Apellido: 'Irungaray',
-                Edad:36,
-                Nombre: 'Juan',
-                Telefono:'42142727'
-            },
+            data: data1
         };
 
         // Saves the entity
         await datastore.save(task);
         console.log(`Saved ${task.key.name}: ${task.data.description}`);
         }
-        quickStart().catch(console.error);
-
-
+        quickStart(params).catch(console.error);
     }
     else{
         res.status(200).send({message:"no entro a post"});
