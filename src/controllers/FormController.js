70ce4d83de76ea28d63680d85905f91c79687b9a
irunga1 <jirungaray@redpronet.com>
2019-03-17 17:47:25 -0600

    comentando console.logs

--- src/controllers/FormController.js
+++ src/controllers/FormController.js
@@ -3,18 +3,18 @@ var path = require('path');
 var utileria = require("../libs/utileria");
 
 function form1(req,res){
-    console.log(__dirname);
+    //console.log(__dirname);
     var utObject = new utileria;
     var ndirname =utObject.removeLastString(__dirname);
-    console.log(ndirname);
+    //console.log(ndirname);
     res.sendFile(path.join(ndirname+ 'views/form.html'));
     //res.status(200).end("<h1><span>hola mundo</span></h1>");
 }
 function index(req,res) {
-    console.log(__dirname);
+    //console.log(__dirname);
     var utObject = new utileria;
     var ndirname =utObject.removeLastString(__dirname);
-    console.log(ndirname);
+    //console.log(ndirname);
     res.sendFile(path.join(ndirname+ 'views/index.html'));
 
 }
--- src/controllers/OperacionController.js
+++ src/controllers/OperacionController.js
@@ -2,7 +2,9 @@
 //var  gcds = require('@google-cloud/datastore');
 
 
-const {Storage} = require('@google-cloud/storage');
+const {
+    Storage
+} = require('@google-cloud/storage');
 
 // Instantiates a client. If you don't specify credentials when constructing
 // the client, the client library will look for credentials in the
@@ -10,68 +12,57 @@ const {Storage} = require('@google-cloud/storage');
 
 
 
-function operate(req, res){
-    var params = req.body;    
-    console.log(params);    
-    if((params.Nombre !=="") && (params.Apellido !=="") && (params.Telefono !=="")&& (params.Edad !=="")){
-        
+function operate(req, res) {
+    var params = req.body;
+    console.log(params);
+    if ((params.Nombre !== "") && (params.Apellido !== "") && (params.Telefono !== "") && (params.Edad !== "")) {
         var btoa = require('btoa');
-        //console.log(params);
-        const {Datastore} = require('@google-cloud/datastore');
+        const {
+            Datastore
+        } = require('@google-cloud/datastore');
         async function quickStart(data1) {
             // Your Google Cloud Platform project ID
             const projectId = 'cloudit4data';
             // Creates a client
             const datastore = new Datastore({
-            projectId: projectId,
-        });
-        // The kind for the new entity
-        const kind = 'Task2';
-        // The name/ID for the new entity
-        var a = new Date();
-        var b = btoa(a);
-        const name = b;
-        // The Cloud Datastore key for the new entity
-        const taskKey = datastore.key([kind, name]);
-        // Prepares the new entity
-        // const task = {
-        //     key: taskKey,
-        //     data: {
-        //         Apellido: 'Irungaray',
-        //         Edad:36,
-        //         Nombre: 'Juan',
-        //         Telefono:'42142727'
-        //     },
-        // };
-        const task = {
-            key: taskKey,
-            data: data1
-        };
+                projectId: projectId,
+            });
+            // The kind for the new entity
+            const kind = 'Task2';
+            // The name/ID for the new entity
+            var a = new Date();
+            var b = btoa(a);
+            const name = b;
+            // The Cloud Datastore key for the new entity
+            const taskKey = datastore.key([kind, name]);
+            const task = {
+                key: taskKey,
+                data: data1
+            };
 
-        // Saves the entity
-        await datastore.save(task);
-        console.log(`Saved ${task.key.name}: ${task.data.description}`);
+            // Saves the entity
+            await datastore.save(task);
+            console.log(`Saved ${task.key.name}: ${task.data.description}`);
         }
         quickStart(params).catch(console.error);
         //res.status(200).send({message:"entro a post"});
         var path = require('path');
         var utileria = require("../libs/utileria");
         var utObject = new utileria;
-        var ndirname =utObject.removeLastString(__dirname);
-        console.log(ndirname);
-        res.sendFile(path.join(ndirname+ 'views/redirect.html'));
+        var ndirname = utObject.removeLastString(__dirname);
+        //console.log(ndirname);
+        res.sendFile(path.join(ndirname + 'views/redirect.html'));
 
-    }
-    else{
+    } else {
         //res.status(200).send({message:"no entro a post"});
         var path = require('path');
         var utileria = require("../libs/utileria");
         var utObject = new utileria;
-        var ndirname =utObject.removeLastString(__dirname);
-        console.log(ndirname);
-        res.sendFile(path.join(ndirname+ 'views/error.html'));
+        var ndirname = utObject.removeLastString(__dirname);
+        //console.log(ndirname);
+        res.sendFile(path.join(ndirname + 'views/error.html'));
     }
-    
+
 }
 module.exports = {
     operate
